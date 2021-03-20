2db20d699fd84d0e2ea51fa402d9927a58d48fc4
Vakarev <vvk9744@gmail.com>
2021-03-20 17:50:21 +0300

    Защита

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 let buttonActive = document.querySelector('.contacts__button');
 let buttonClose = document.querySelector('.modal-close');
 let modal = document.querySelector('.modal');
-buttonActive.onclick = function() {
+
+buttonActive.addEventListener("click", function (evt) {
+    evt.preventDefault();
     modal.classList.remove('modal-none'); 
-  }
-buttonClose.onclick = function() {
-    modal.classList.add('modal-none'); 
-  }
+});
+
+buttonClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modal.classList.remove('modal-none'); 
+  modal.classList.add('modal-none');
+});
